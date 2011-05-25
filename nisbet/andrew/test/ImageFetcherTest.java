4e1ce1904ba91ff6deda7a20fe0024af0209a2b6
nisbetorama <nisbetorama@gmail.com>
2011-05-25 20:53:46 +0000

    Tests updated to reflect code changes.

--- nisbet/andrew/test/ImageFetcherTest.java
+++ nisbet/andrew/test/ImageFetcherTest.java
@@ -5,6 +5,9 @@ package nisbet.andrew.test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.net.UnknownHostException;
+
 import nisbet.andrew.service.ImageFetcher;
 
 import org.junit.Test;
@@ -20,9 +23,41 @@ public class ImageFetcherTest {
 	 */
 	@Test
 	public void testImageFetcher() {
-		ImageFetcher imageFetcher = new ImageFetcher( "http://en.wikipedia.org/wiki/Apiaceae", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
+		ImageFetcher imageFetcher = null;
+		try {
+			imageFetcher = new ImageFetcher( "http://en.wikipedia.org/wiki/Apiaceae", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
+		} catch (UnknownHostException e) {
+			return;
+		}
 		assertNotNull( imageFetcher );
 		System.out.println( imageFetcher.getImageURL() );
+		assertTrue( imageFetcher.isBigEnough() );
+		
+		try {
+			imageFetcher = new ImageFetcher( "http://www.p.fds", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
+		} catch (UnknownHostException e) {
+			System.err.println( "The host: '" + "http://www.p.fds" + "' is unknown." );
+		}
+		assertNotNull( imageFetcher );
+		System.out.println( imageFetcher.getImageURL() );
+		assertTrue( imageFetcher.isBigEnough() == false );
+	}
+	
+	@Test
+	public void testDownloadImage()
+	{
+		ImageFetcher imageFetcher = null;
+		try {
+			imageFetcher = new ImageFetcher( "http://en.wikipedia.org/wiki/Apiaceae", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
+		} catch (UnknownHostException e) {
+			return;
+		}
+		assertNotNull( imageFetcher );
+		System.out.println( imageFetcher.getImageURL() );
+		assertTrue( imageFetcher.isBigEnough() );
+		imageFetcher.download();
+		File file = new File( "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
+		assertTrue( file.exists() );
 	}
 
 }
