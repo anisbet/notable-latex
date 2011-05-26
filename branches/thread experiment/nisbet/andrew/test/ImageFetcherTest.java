cf8bf86f59975870b4853a2db56d6f6351500a1f
nisbetorama@gmail.com <nisbetorama@gmail.com@1e290836-61b3-dff7-6fdd-1894570d6131>
2011-05-26 04:52:53 +0000

    Tests updated to reflect code changes.

--- branches/thread experiment/nisbet/andrew/test/ImageFetcherTest.java
+++ branches/thread experiment/nisbet/andrew/test/ImageFetcherTest.java
@@ -21,43 +21,68 @@ public class ImageFetcherTest {
 	/**
 	 * Test method for {@link nisbet.andrew.service.ImageFetcher#ImageFetcher(java.lang.String, java.lang.String)}.
 	 */
+//	@Test
+//	public void testImageFetcher() {
+//		ImageFetcher imageFetcher = null;
+//		try {
+//			imageFetcher = new ImageFetcher( "http://en.wikipedia.org/wiki/Apiaceae", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
+//		} catch (UnknownHostException e) {
+//			return;
+//		}
+//		assertNotNull( imageFetcher );
+//		System.out.println( imageFetcher.getImageURL() );
+//		assertTrue( imageFetcher.isBigEnough() );
+//		
+//		try {
+//			imageFetcher = new ImageFetcher( "http://www.p.fds", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
+//		} catch (UnknownHostException e) {
+//			System.err.println( "The host: '" + "http://www.p.fds" + "' is unknown." );
+//		}
+//		assertNotNull( imageFetcher );
+//		System.out.println( imageFetcher.getImageURL() );
+//		assertTrue( imageFetcher.isBigEnough() == false );
+//	}
+	
+//	@Test
+//	public void testDownloadImage()
+//	{
+//		ImageFetcher imageFetcher = null;
+//		try {
+//			imageFetcher = new ImageFetcher( "http://en.wikipedia.org/wiki/Apiaceae", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
+//		} catch (UnknownHostException e) {
+//			return;
+//		}
+//		assertNotNull( imageFetcher );
+//		System.out.println( imageFetcher.getImageURL() );
+//		assertTrue( imageFetcher.isBigEnough() );
+//		imageFetcher.download();
+//		File file = new File( "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
+//		assertTrue( file.exists() );
+//	}
+
 	@Test
-	public void testImageFetcher() {
-		ImageFetcher imageFetcher = null;
-		try {
-			imageFetcher = new ImageFetcher( "http://en.wikipedia.org/wiki/Apiaceae", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
-		} catch (UnknownHostException e) {
-			return;
-		}
-		assertNotNull( imageFetcher );
-		System.out.println( imageFetcher.getImageURL() );
-		assertTrue( imageFetcher.isBigEnough() );
-		
-		try {
-			imageFetcher = new ImageFetcher( "http://www.p.fds", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
-		} catch (UnknownHostException e) {
-			System.err.println( "The host: '" + "http://www.p.fds" + "' is unknown." );
-		}
-		assertNotNull( imageFetcher );
-		System.out.println( imageFetcher.getImageURL() );
-		assertTrue( imageFetcher.isBigEnough() == false );
+	public void testSize()
+	{
+		String size = "100px";
+		//System.out.println( ImageFetcher.stripScale(size) );
+		assertTrue( ImageFetcher.stripScale(size).equalsIgnoreCase("100"));
+		assertTrue( ImageFetcher.stripScale("word").equalsIgnoreCase(""));
+		assertTrue( ImageFetcher.stripScale("1px").equalsIgnoreCase("1"));
+		assertTrue( ImageFetcher.stripScale("1000000cm").equalsIgnoreCase("1000000"));
+		assertTrue( ImageFetcher.stripScale("1").equalsIgnoreCase("1"));
+		assertTrue( ImageFetcher.stripScale("1 px").equalsIgnoreCase("1"));
+		assertTrue( ImageFetcher.stripScale("").equalsIgnoreCase(""));
+		assertTrue( ImageFetcher.stripScale(null).equalsIgnoreCase(""));
 	}
 	
 	@Test
-	public void testDownloadImage()
+	public void testOptimalImage()
 	{
-		ImageFetcher imageFetcher = null;
-		try {
-			imageFetcher = new ImageFetcher( "http://en.wikipedia.org/wiki/Apiaceae", "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
-		} catch (UnknownHostException e) {
-			return;
-		}
-		assertNotNull( imageFetcher );
-		System.out.println( imageFetcher.getImageURL() );
-		assertTrue( imageFetcher.isBigEnough() );
-		imageFetcher.download();
-		File file = new File( "Umbelliferae-apium-daucus-foeniculum-eryngium-petroselinum.jpg" );
-		assertTrue( file.exists() );
+		ImageFetcher fetcher = null;
+		fetcher = new ImageFetcher("http://en.wikipedia.org/wiki/Apiaceae");
+		assertTrue(fetcher.download());
+		assertTrue( ImageFetcher.testImageExists( fetcher.getImageName() ) );
+		System.out.println( fetcher.getImageName());
 	}
 
 }
