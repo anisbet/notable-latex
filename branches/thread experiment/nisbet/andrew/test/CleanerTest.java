ca42bbbc90478f0c3ecb0a56320c93bb0ca60ede
nisbetorama <nisbetorama@gmail.com>
2011-07-18 18:26:11 +0000

    Moved CleanerTest to the test directory and added file output print statements.

--- branches/thread experiment/nisbet/andrew/test/CleanerTest.java
+++ branches/thread experiment/nisbet/andrew/test/CleanerTest.java
@@ -10,6 +10,7 @@ public class CleanerTest {
 
 	@Test
 	public void testClean() {
+		System.out.println(">>>" + Cleaner.cleanSpecialCharacters("1.2.3.jpg") + "<<<" );
 		System.out.println(">>>" + Cleaner.cleanSpecialCharacters("300px-0453_-_Roma2C_Museo_d._civiltC3A0_romana_-_Sarcofago_Mattei_Foto_Giovanni_Dall27Orto2C_12-Apr-2008.jpg") + "<<<" );
 	}
 
