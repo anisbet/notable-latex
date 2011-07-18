8288354b9fcdae2f7b66a7dffdcbc5cf004d00f6
nisbetorama@gmail.com <nisbetorama@gmail.com@1e290836-61b3-dff7-6fdd-1894570d6131>
2011-07-18 16:56:36 +0000

    Fixed Clean and renamed function for clarity. Also moved test to the test dir, and fixed algorithm which failed on some file names. Broadened the definition of special char so that we trim underscores and % and .

---
+++ branches/thread experiment/nisbet/andrew/test/CleanerTest.java
@@ -0,0 +1,16 @@
+package nisbet.andrew.test;
+
+import static org.junit.Assert.*;
+
+import nisbet.andrew.latex.Cleaner;
+
+import org.junit.Test;
+
+public class CleanerTest {
+
+	@Test
+	public void testClean() {
+		System.out.println(">>>" + Cleaner.cleanSpecialCharacters("300px-0453_-_Roma2C_Museo_d._civiltC3A0_romana_-_Sarcofago_Mattei_Foto_Giovanni_Dall27Orto2C_12-Apr-2008.jpg") + "<<<" );
+	}
+
+}
